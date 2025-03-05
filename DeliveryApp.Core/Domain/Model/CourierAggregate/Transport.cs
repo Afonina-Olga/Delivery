e536cb536917f726a1aedd0eb29bae23db8089d4
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-06 01:38:27 +0300

    Модуль 3. Entity

---
+++ DeliveryApp.Core/Domain/Model/CourierAggregate/Transport.cs
@@ -0,0 +1,93 @@
+﻿using CSharpFunctionalExtensions;
+using Primitives;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DeliveryApp.Core.Domain.Model.CourierAggregate
+{
+	public class Transport : Entity<int>
+	{
+		public static readonly Transport Pedestrian = new(1, nameof(Pedestrian), 1);
+		public static readonly Transport Bicycle = new(2, nameof(Bicycle), 2);
+		public static readonly Transport Car = new(3, nameof(Car), 3);
+
+		/// <summary>
+		/// Конструктор
+		/// </summary>
+		[ExcludeFromCodeCoverage]
+		private Transport()
+		{
+
+		}
+
+		/// <summary>
+		/// Конструктор
+		/// </summary>
+		/// <param name="id">Идентификатор</param>
+		/// <param name="name">Название</param>
+		/// <param name="speed">Скорость</param>
+		private Transport(int id, string name, int speed) : this()
+		{
+			Id = id;
+			Name = name;
+			Speed = speed;
+		}
+
+		/// <summary>
+		/// Название
+		/// </summary>
+		public string Name { get; }
+
+		/// <summary>
+		/// Скорость
+		/// </summary>
+		public int Speed { get; }
+
+		/// <summary>
+		/// Список всех значений списка
+		/// </summary>
+		/// <returns>Значения списка</returns>
+		public static IEnumerable<Transport> List()
+		{
+			yield return Pedestrian;
+			yield return Bicycle;
+			yield return Car;
+		}
+
+		/// <summary>
+		/// Получить транспорт по названию
+		/// </summary>
+		/// <param name="name">Название</param>
+		/// <returns>Транспорт</returns>
+		public static Result<Transport, Error> FromName(string name)
+		{
+			var state = List()
+				.SingleOrDefault(s => string.Equals(s.Name, name, StringComparison.CurrentCultureIgnoreCase));
+			if (state == null) return Errors.TransportIsWrong();
+			return state;
+		}
+
+		/// <summary>
+		/// Получить транспорт по идентификатору
+		/// </summary>
+		/// <param name="name">Название</param>
+		/// <returns>Транспорт</returns>
+		public static Result<Transport, Error> FromId(int id)
+		{
+			var state = List().SingleOrDefault(s => s.Id == id);
+			if (state == null) return Errors.TransportIsWrong();
+			return state;
+		}
+
+		/// <summary>
+		/// Ошибки, которые может возвращать сущность
+		/// </summary>
+		public static class Errors
+		{
+			public static Error TransportIsWrong()
+			{
+				return new Error($"{nameof(Transport).ToLowerInvariant()}.is.wrong",
+					$"Не верное значение. Допустимые значения: {nameof(Transport).ToLowerInvariant()}: {string.Join(",", List().Select(s => s.Name))}");
+			}
+		}
+	}
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/Model/CourierAggregate/TransportShould.cs
@@ -0,0 +1,88 @@
+﻿using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Model.CourierAggregate
+{
+	public class TransportShould
+	{
+		[Theory]
+		[InlineData("Bicycle")]
+		[InlineData("Pedestrian")]
+		[InlineData("Car")]
+		public void CanGetTransportByNameWhenNameIsCorrect(string name)
+		{
+			//Arrange
+
+			//Act
+			var result = Transport.FromName(name);
+
+			//Assert
+			result.IsSuccess.Should().BeTrue();
+			result.Value.Name.Should().Be(name);
+		}
+
+		[Theory]
+		[InlineData("Skier")]
+		[InlineData("Motorcyclist")]
+		public void ReturnsErrorWhenNameIsInCorrect(string name)
+		{
+			//Arrange
+
+			//Act
+			var result = Transport.FromName(name);
+
+			//Assert
+			result.IsSuccess.Should().BeFalse();
+			result.Error.Should().Be(Transport.Errors.TransportIsWrong());
+		}
+
+		[Theory]
+		[InlineData(1)]
+		[InlineData(2)]
+		[InlineData(3)]
+		public void CanGetTransportByIdWhenIdIsCorrect(int id)
+		{
+			// Arrange
+
+			// Act
+			var result = Transport.FromId(id);
+
+			// Assert
+			result.IsSuccess.Should().BeTrue();
+			result.Value.Id.Should().Be(id);
+		}
+
+		[Theory]
+		[InlineData(10)]
+		[InlineData(0)]
+		[InlineData(4)]
+		public void ReturnsErrorWhenIdIsInCorrect(int id)
+		{
+			// Arrange
+
+			// Act
+			var result = Transport.FromId(id);
+
+			// Assert
+			result.IsSuccess.Should().BeFalse();
+			result.Error.Should().Be(Transport.Errors.TransportIsWrong());
+		}
+
+		// Два транспорта должны быть равны, если их ID равны
+		// TODO Не поняла, нужен ли этот тест вообще
+		[Fact]
+		public void TwoTransportsAreEqualWhenIdsAreEqual()
+		{
+			// Arrange
+			var transport1 = Transport.Car;
+			var transport2 = Transport.Car;
+
+			// Act
+			var result = transport1 == transport2;
+
+			// Assert
+			result.Should().BeTrue();
+		}
+	}
+}
