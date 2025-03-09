362c996eeb4bd1bb804ea8ee6d914dadd4e15241
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-10 00:32:26 +0300

    Order

---
+++ DeliveryApp.Core/Domain/Model/CourierAggregate/Courier.cs
@@ -0,0 +1,13 @@
+﻿using Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Core.Domain.Model.CourierAggregate
+{
+	public class Courier : Aggregate<Guid>
+	{
+	}
+}
